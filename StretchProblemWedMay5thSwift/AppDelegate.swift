d73fc3fb668c0bdd5a3f1646b6283356ea2931f1
Doug Voss <douglas.c.voss@gmail.com>
2015-05-06 09:50:44 -0600

    initial commit

---
+++ StretchProblemWedMay5thSwift/AppDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  AppDelegate.swift
+//  StretchProblemWedMay5thSwift
+//
+//  Created by Douglas Voss on 5/6/15.
+//  Copyright (c) 2015 Doug. All rights reserved.
+//
+/*
+studentCan: iterate through numbers, find multiples, and add them together
+
+* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6, and 9.  The sum of these multiples is 23.
+
+* Create a method that will find the sum of all the multiples of 3 or 5 below an integer passed into it
+
+* Return the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func allNumbersThatAreMultiplesOf3or5BelowThisNumber(#number: Int) -> Int {
+        /*var someInts : Array = [Int]()
+        someInts.append(3)
+        someInts.append(5)
+        
+        for (var i=0; i<someInts.count; i++) {
+            println("someInts[\(i)]==\(someInts[i])")
+        }*/
+        
+        var multsOf3 : Array = [Int]()
+        var sum : Int = 0
+        for (var i=1; (i*3) < number; i++) {
+            multsOf3.append(i*3)
+            sum += multsOf3[i-1]
+            println("multsOf3[\(i-1)]==\(multsOf3[i-1]); sum=\(sum)")
+        }
+        
+        var multsOf5 : Array = [Int]()
+        for (var i=1; (i*5) < number; i++) {
+            multsOf5.append(i*5)
+            sum += multsOf5[i-1]
+            println("multsOf5[\(i-1)]==\(multsOf5[i-1]); sum=\(sum)")
+        }
+        
+        return sum
+        
+    }
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //println("function(10) returns \(allNumbersThatAreMultiplesOf3or5BelowThisNumber(number: 10))")
+        println("function(1000) returns \(allNumbersThatAreMultiplesOf3or5BelowThisNumber(number: 1000))")
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
